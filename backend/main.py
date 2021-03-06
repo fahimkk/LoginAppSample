0d648c25389ce50c5ac2c85be9bf01d96d83d1d6
fahimkk <fahimasharafkk@gmail.com>
2021-03-06 10:05:54 +0530

    Deal with sign up and sign in

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,78 @@
 from flask import Flask, request
 import sqlite3
+import json
 
 app = Flask(__name__)
 
-def addData(username, password):
+
+def addData(username, email, password):
+    print("Adding Data")
     conn = sqlite3.connect('loginapp.db')
     c = conn.cursor()
-    c.execute("INSERT INTO credentials VALUES ('{}','{}')".format(username, password))
+    c.execute("INSERT INTO credentials VALUES ('{}','{}','{}')".format(
+        username, email, password))
     conn.commit()
     conn.close()
+    print("Adding Data Successful")
+    return 'success'
+
+
+def verifyData(email, password):
+    print('verify data')
+    print(email, password)
+    conn = sqlite3.connect('loginapp.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM credentials WHERE email='{}'".format(email))
+    conn.commit()
+    tup = c.fetchone()
+    conn.close()
+    status_dict = {}
+    if not tup:
+        status_dict["status"] = "nil"
+        status_json = json.dumps(status_dict)
+        print('Please Sign UP')
+        print(status_json)
+        return status_json
+    print('Checking Database')
+    db_password = tup[1]
+    if db_password != password:
+        status_dict["status"] = "incorrect"
+        status_json = json.dumps(status_dict)
+        print('incorrect password')
+        return status_json
+    print("success")
+    status_dict["status"] = "success"
+    status_json = json.dumps(status_dict)
+    return status_json
 
 
 @app.route('/', methods=['POST', 'GET'])
-def hello():
-    print('before hello')
+def signIn():
+    print('Sing in')
+    if request.method == 'POST':
+        # if data sent as json, ie application/json, the we can't get it
+        # by using request.form, we have to use either .data or .get_json()
+        # get_json will return a dict
+        data = request.get_json()
+        email = data["email"]
+        password = data["password"]
+        print(email, password)
+        return verifyData(email, password)
+
+
+@app.route('/signUp', methods=['POST', 'GET'])
+def signUp():
+    print('Welcome to SignUp')
     if request.method == 'POST':
         # if data sent as json, ie application/json, the we can't get it
         # by using request.form, we have to use either .data or .get_json()
         # get_json will return a dict
         data = request.get_json()
         username = data["username"]
+        email = data["email"]
         password = data["password"]
-        addData(username, password)
-        return "OK"
+        print(email, password)
+        return addData(username, email, password)
 
 
 if __name__ == '__main__':
@@ -34,6 +84,7 @@ conn = sqlite3.connect('loginapp.db')
 c = conn.cursor()
 conn.execute('''CREATE TABLE credentials
          (username text,
+         email text,
          password text )''')
 conn.commit()
 conn.close()
